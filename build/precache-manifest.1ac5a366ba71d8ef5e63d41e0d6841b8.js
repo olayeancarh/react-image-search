6f3844235f04f11a122a9c03e01536f1d86acf73
olayeancarh <akejuolayinka016@gmail.com>
2019-08-21 13:16:44 +0100

    Setup  of react-image-search successful

---
+++ build/precache-manifest.1ac5a366ba71d8ef5e63d41e0d6841b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79062749dbce603452dc60f70a1812f0",
+    "url": "/react-image-search/index.html"
+  },
+  {
+    "revision": "50cbd07a96a23483082c",
+    "url": "/react-image-search/static/css/main.a45e65a8.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/react-image-search/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "50cbd07a96a23483082c",
+    "url": "/react-image-search/static/js/main.b782cb19.chunk.js"
+  },
+  {
+    "revision": "44dc1c358429f9c9d8c3",
+    "url": "/react-image-search/static/js/runtime~main.9c665905.js"
+  }
+]);
